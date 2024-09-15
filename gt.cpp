8791841f040e78b0ec5ac1827b73722eb92e040f
turbileg12 <turbilegbatbileg1@gmail.com>
2024-09-16 01:03:38 +0800

    ss

--- gt.cpp
+++ gt.cpp
@@ -6,11 +6,13 @@ int main() {
 	int n;
 	cin >> n;
 
+	int s = 0;
 	for (int i = 1; i <= n; i++) {
 		cout << i << endl;
+		s++;
 	}
 
-	cout << ":)" << endl;
+	cout << "s" << endl;
 
 	return 0;
 }
